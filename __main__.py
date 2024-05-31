6a4ecedd8ed3fbd49a8b307755e56daf3becc272
evangerva <colger88@gmail.com>
2024-05-31 16:32:52 +0200

    added basic api code

---
+++ __main__.py
@@ -0,0 +1,8 @@
+import time
+import similar
+
+
+
+while 1:
+    print(similar.similarGet('http://google.com'))
+    time.sleep(3)
---
+++ similar.py
@@ -0,0 +1,17 @@
+try:
+    from requests import get
+    from urllib.parse import urlparse
+except ImportError as err:
+    print(f"Failed to import required modules {err}")
+
+def similarGet(website):
+    domain = '{uri.netloc}'.format(uri=urlparse(website))
+    domain = domain.replace("www.", "")
+    ENDPOINT = 'https://data.similarweb.com/api/v1/data?domain=' + domain
+    resp = get(ENDPOINT)
+
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        resp.raise_for_status()
+        return False
