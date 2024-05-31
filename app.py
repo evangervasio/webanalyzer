a09d0f1129257e0c9f96d9698f9ee4b3a686edd2
evangerva <colger88@gmail.com>
2024-05-31 22:16:11 +0200

    added working exchange dedicated page

--- app.py
+++ app.py
@@ -5,28 +5,38 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    raw_data = None
-    coin = ""
-    exchange = ""
-
-    # Fetch all exchanges to populate the combobox
     api_caller = CoinloreApi()
     exchanges_data = api_caller.getAllExchanges()
+    names = []
 
-    if request.method == "POST":
-        coin = request.form["coin"]
-        exchange = request.form["exchange"]
-
-        api_caller = CoinloreApi(coin, exchange)
-        raw_data = api_caller.determine()
-
-    #get names to put in comboboxs
-    names=[]
     for exc in exchanges_data:
-        exchange=(exchanges_data[exc])
+        exchange = exchanges_data[exc]
         names.append(exchange["name"])
 
-    return render_template("index.html", raw_data=raw_data, exchanges=names)
+    return render_template("index.html", exchanges=names)
+
+@app.route("/exchange", methods=["POST"])
+def exchange():
+    coin = request.form.get("coin", "")
+    exchange_name = request.form["exchange"]
+    exchange_id = None
+
+    api_caller = CoinloreApi()
+
+    # Fetch all exchanges to find the selected one by name
+    exchanges_data = api_caller.getAllExchanges()
+
+    for exc in exchanges_data:
+        if exchanges_data[exc]["name"] == exchange_name:
+            exchange_id = exchanges_data[exc]["id"]
+            break
+
+    if exchange_id:
+        api_caller = CoinloreApi(coin, exchange_id)
+        exchange_data = api_caller.getExchange()
+        return render_template("exchange.html", exchange_data=exchange_data)
+    else:
+        return "Exchange not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
